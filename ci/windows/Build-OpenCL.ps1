da53ae57c8ce490ea09b6bc277e2e6531532915b
TibixDev <tibix@fhs.sh>
2026-08-12 12:57:32 +0300

    deps: pin reviewed D3D11 sharing fixes

--- ci/windows/Build-OpenCL.ps1
+++ ci/windows/Build-OpenCL.ps1
@@ -3,7 +3,7 @@ param(
     [string]$SourceRoot = "C:\clvk-src",
     [string]$BuildRoot = "C:\clvk-build",
     [string]$ClvkRepository = "https://github.com/winboat-org/clvk-helios.git",
-    [string]$ClvkCommit = "617b6e87b1e228dff04a04e9411c390ea1928a4c",
+    [string]$ClvkCommit = "783c0dc6c38d367791e7ff5e377d870167e02f1f",
     # Patches applied to the clspv submodule after checkout. clvk-helios carries
     # clspv as a submodule of upstream google/clspv, so Helios-local compiler
     # fixes live here as patches until there is enough divergence to justify a
